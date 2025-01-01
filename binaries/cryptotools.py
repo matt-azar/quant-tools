687f3dc635c9a32916edcdbd64f3c75a8f193dfd
matt-azar <azarmatthewj@gmail.com>
2025-01-01 17:57:01 -0500

    Improved error handling

--- binaries/cryptotools.py
+++ binaries/cryptotools.py
@@ -1,17 +1,18 @@
 import requests
 
 def cryptoprice(coin = "BTC") -> float:
-    match coin:
-        case "BTC":
-            url = "https://www.cfbenchmarks.com/data/indices/BRTI"
-        case "ETH":
-            url = "https://www.cfbenchmarks.com/data/indices/ETHUSD_RTI"
-        case _:
-            print("Enter a valid coin.")
-            return 0.0
+    urls = {
+        "BTC": "https://www.cfbenchmarks.com/data/indices/BRTI",
+        "ETH": "https://www.cfbenchmarks.com/data/indices/ETHUSD_RTI"
+    }
+    if coin not in urls:
+        raise ValueError("Unsupported coin.")
+    url = urls[coin]
     response = requests.get(url)
     tgt = "font-semibold tabular-nums md:text-2xl\">$"
     idx = response.text.find(tgt) + len(tgt)
+    if idx == -1:
+        raise Exception("Unable to parse price.")
     s = response.text[idx:idx+10]
     while len(s) and not s[-1].isdigit():
         s = s[:-1]
